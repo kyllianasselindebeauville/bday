a96c6529a017919d4676960450fce2f2076b0a41
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-10-14 20:35:44 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='birthday_tracker',
+    description="Keep track of your loved ones' birthdays",
+    author='Kyllian Asselin de Beauville',
+    author_email='kyllianasselindebeauville@gmail.com',
+    url='https://github.com/kyllianasselindebeauville/birthday-tracker',
+    packages=['birthday_tracker'],
+    license='MIT',
+    python_requires='>=3.7',
+)
