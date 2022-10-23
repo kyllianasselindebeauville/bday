db7ef868d49e414b13cc7b848e11962f1dbd3f83
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-10-23 21:42:24 +0200

    Add functions to load from and save to the data file

--- birthday_tracker/birthdays.py
+++ birthday_tracker/birthdays.py
@@ -12,3 +12,22 @@ def get_filepath(file: str = None) -> str:
         return get_default_filepath()
 
     return os.path.join(os.getenv('HOME'), '.birthday-tracker', file)
+
+
+def get(file: str = None) -> pd.DataFrame:
+    filepath = get_filepath(file)
+
+    if os.path.exists(filepath):
+        df = pd.read_csv(filepath, index_col='ID')
+    else:
+        df = pd.DataFrame(columns=['Name', 'Birthdate'])
+
+    return df
+
+
+def save(df: pd.DataFrame, file: str = None) -> None:
+    filepath = get_filepath(file)
+    directory, file = os.path.split(filepath)
+
+    os.makedirs(directory, exist_ok=True)
+    df.to_csv(filepath, index_label='ID')
