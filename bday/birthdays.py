0b8c292788cd3932aebef163b89060f551e91563
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-10-24 06:32:42 +0200

    Add `_get_default_directory()`

--- bday/birthdays.py
+++ bday/birthdays.py
@@ -3,15 +3,19 @@ import os
 import pandas as pd
 
 
+def _get_default_directory() -> str:
+    return os.path.join(os.getenv('HOME'), '.bday')
+
+
 def _get_default_filepath() -> str:
-    return os.path.join(os.getenv('HOME'), '.bday', 'birthdays.csv')
+    return os.path.join(_get_default_directory(), 'birthdays.csv')
 
 
 def _get_filepath(file: str = None) -> str:
     if file is None:
         return _get_default_filepath()
 
-    return os.path.join(os.getenv('HOME'), '.bday', file)
+    return os.path.join(_get_default_directory(), file)
 
 
 def get(file: str = None) -> pd.DataFrame:
