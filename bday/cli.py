9f73a1bfb8e8bf2e0bb7f904908e806a349ae106
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-10-29 17:49:36 +0200

    Move the CLI to cli.py

---
+++ bday/cli.py
@@ -0,0 +1,39 @@
+import argparse
+import inspect
+
+
+def spam(args):
+    print(f'{inspect.stack()[0][3]} <- {args}')
+
+
+def ham(args):
+    print(f'{inspect.stack()[0][3]} <- {args}')
+
+
+def eggs(args):
+    print(f'{inspect.stack()[0][3]} <- {args}')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog='bday',
+                                     description='Track your birthdays.')
+
+    subparsers = parser.add_subparsers(title='commands')
+
+    # Default function
+    parser.set_defaults(func=spam)
+
+    # Parser for the spam command
+    parser_spam = subparsers.add_parser('spam')
+    parser_spam.set_defaults(func=spam)
+
+    # Parser for the ham command
+    parser_ham = subparsers.add_parser('ham')
+    parser_ham.set_defaults(func=ham)
+
+    # Parser for the eggs command
+    parser_eggs = subparsers.add_parser('eggs')
+    parser_eggs.set_defaults(func=eggs)
+
+    args = parser.parse_args()
+    return args
