bef325b0a760c61827b7c658714aadfdef87033f
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-10-30 19:46:07 +0100

    Add a subset to drop missing and duplicate values

--- bday/birthdays.py
+++ bday/birthdays.py
@@ -45,8 +45,8 @@ def save(df: pd.DataFrame, file: str = None) -> None:
     filepath = _get_filepath(file)
     directory, file = os.path.split(filepath)
 
-    df.dropna(inplace=True)
-    df.drop_duplicates(inplace=True)
+    df.dropna(subset=['Name', 'Birthdate'], inplace=True)
+    df.drop_duplicates(subset=['Name', 'Birthdate'], inplace=True)
 
     os.makedirs(directory, exist_ok=True)
     df.to_csv(filepath, index_label='ID')
